467c782a6a92477ce4edb4bdcc55e2341e665f91
dad <dominic.colson@hotmail.co.uk>
2017-07-06 18:17:23 +0100

    working 2

--- dataConnector.js
+++ dataConnector.js
@@ -3,22 +3,62 @@
  */
 
 var pg = require('pg');
-var pgp = require('pg-promise');
 
-var conString = "postgres://postgres:postgres@52.178.47.198:5432/nnm";
-var client = new pg.Client(conString);
+//var x = getRows();
+//console.log("This is a " + getRowCount());
+getRows();
+getRowCount();
+writeRows();
 
+//console.log(x);
 
+function getRowCount(){
+    var conString = "postgres://postgres:postgres@52.178.47.198:5432/nnm";
+    var client = new pg.Client(conString);
+    var columnCount = "";
 
+    client.connect();
 
-client.connect();
+    client.query("SELECT * FROM public.nms_author", function(err, result){
+        columnCount = result.rows.length;
+        console.log("LDL %d", columnCount);
+        client.end();
+    });
 
-var query = client.query('SELECT * FROM public.nms_author');
+}
 
-query.on('row', function(row){
-   console.log(row);
-});
+function getRows(){
+    var conString = "postgres://postgres:postgres@52.178.47.198:5432/nnm";
+    var client = new pg.Client(conString);
 
-query.on('end', function(){
-    client.end();
-});
+    client.connect();
+
+    var query = client.query('SELECT * FROM public.nms_author');
+
+    query.on('row', function(row){
+        console.log(row);
+    });
+
+    query.on('end', function(){
+        client.end();
+    });
+}
+
+function writeRows(){
+    var outConString = "postgres://postgres:postgres@52.178.47.198:5432/nhs";
+    var client = new pg.Client(outConString);
+    var queryString = "INSERT INTO test(rowcount) VALUES('77')";
+
+    client.connect();
+
+    var query= client.query(queryString);
+
+    query.on('row', function(row, result){
+        console.log(result);
+        console.log(row);
+    });
+
+    query.on('end', function(){
+        client.end();
+    });
+}
